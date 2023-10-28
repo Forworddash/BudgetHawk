b896486db5080c1c6d50fe30e171b8afa61f0d04
samuel <samuel.baker@rack-a-tiers.com>
2023-10-27 21:57:51 -0700

    - added test divs to test tailwind

--- app/src/index.jsx
+++ app/src/index.jsx
@@ -8,6 +8,15 @@ const root = createRoot(container);
 
 root.render(
     <React.Fragment>
-        <div className="bg-red-200">Hello world</div>
+        <div className="bg-purple-500">Hello world</div>
+        <div className="flex flex-row space-x-4">
+            <div className="basis-1/2 bg-purple-500 w-12">
+                <p>This is another test div</p>
+            </div>
+            <div className="basis-1/2 bg-purple-500 w-12">
+                <p>this is a test div</p>
+            </div>
+
+        </div>
     </React.Fragment>
 );
