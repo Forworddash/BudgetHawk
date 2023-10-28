87ed0525808526604636d7ee27a9ee430a14fe35
samuel <samuel.baker@rack-a-tiers.com>
2023-10-28 15:21:54 -0700

    - added first tabs

--- app/src/index.jsx
+++ app/src/index.jsx
@@ -11,20 +11,33 @@ root.render(
     // make some tabs and stuff
     // make a grid
     // make a list of tabs
-    <div className="flex justify-center items-center">
+    // <div className="flex justify-center items-center">
+    <div className="">
         <Tabs>
-            <div label='Gator'>
-                See ya later, <em>Alligator</em>!
+            <div label='Dashboard'>
+                <div>
+                    <div className="text-center bg-gray-200">
+                        <p>Test</p>    
+                    </div>    
+                </div>
             </div>
-            <div label='Croc'>
-                After 'while, <em>Crocodile</em>!
+            <div label='Calculators'>
+                <div>
+                    <div className="text-center bg-gray-200">
+                        <p>Test</p>    
+                    </div>    
+                </div>
             </div>
-            <div label='Sarcosuchus'>
-                Nothing to see here, this tab is <em>extinct</em>!
+            <div label='Settings'>
+                <div className="">
+                    <div className="text-center bg-gray-200">
+                        <p>Settings</p>    
+                    </div>    
+                </div>
             </div>
         </Tabs>
         
-        <div className="flex flex-row space-x-4">
+        {/* <div className="flex flex-row space-x-4">
             
             <div className="w-1/2 bg-purple-500 p-4">
                 <p>This is another test div</p>
@@ -32,7 +45,7 @@ root.render(
             <div className="w-1/2 bg-purple-500 p-4">
                 <p>This is a test div</p>
             </div>
-        </div>
+        </div> */}
         
     </div>
 );
